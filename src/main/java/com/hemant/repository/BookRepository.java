324606b287c3de5f5c2f1c13a3fa9e6828f80618
Hemant Kumar Sahu <hemantjava90@gmail.com>
2022-03-20 22:40:22 +0530

    Delete src directory

--- src/main/java/com/hemant/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hemant.repository;
-
-import com.hemant.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface BookRepository extends JpaRepository<Book,Long> {
-}
